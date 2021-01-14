34c978600d8aa47240eab938a87681f2a3a7c59e
Fabien Gallet <64895729+alca-creator@users.noreply.github.com>
2021-01-14 11:27:55 +0100

    Petite progression sur la vue texte - pas certain de la forme et du fond

--- src/View/VueTexte.java
+++ src/View/VueTexte.java
@@ -7,6 +7,7 @@ import java.io.InputStreamReader;
 import java.io.PrintStream;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Scanner;
 
 import Controleur.MsgBox;
 import projetLO02.Jeu;
@@ -17,10 +18,20 @@ public class VueTexte implements Observer, Runnable {
 	
 	public static String QUITTER = "Quit";
 	public static String PROMPT = ">";
+	public static String ADDJOUEUR = "AddJoueur";
+	public static String ADDIA = "AddIA";
+	public static String PLAY = "Play";
+	public static String PIOCHER = "Piocher";
+	public static String PLACER = "Placer";
+	public static String DEPLACER = "Deplacer";
+	public static String FINDETOUR = "Passer";
 	private InputStream input;
 	private PrintStream output;
 	
+	private Jeu jeu;
+	
 	public VueTexte(Jeu jeu, MsgBox msgBox) {
+		this.jeu = jeu;
 		input = System.in;
 		output = System.out;
 		Thread t = new Thread(this);
@@ -52,16 +63,32 @@ public class VueTexte implements Observer, Runnable {
 		String saisie = null;
 		boolean quitter = false;
 		
-		output.println( VueTexte.QUITTER + " pour quitter.");
+		output.println( "Taper " + VueTexte.ADDJOUEUR + " pour ajouter un joueur ou " + VueTexte.ADDIA + " pour ajouter une IA. Taper " + VueTexte.PLAY + " pour jouer ; " + VueTexte.PIOCHER  + " pour piocher ; " + VueTexte.PLACER + " pour placer la carte ; " + VueTexte.DEPLACER + " pour déplacer la carte ; " + VueTexte.FINDETOUR + " pour passer la main ; " + VueTexte.QUITTER + " pour quitter.");
 	
 		do {
 			saisie = this.lireChaine();
 			
 			if(saisie != null) {
-								
-				if(saisie.equals(VueTexte.QUITTER) == true) {
+				if(saisie.equals(VueTexte.ADDJOUEUR) == true) {
+					Scanner name = new Scanner(input);
+					output.println("Entrez le nom du joueur : ");
+					String userName = name.nextLine();
+					jeu.addJoueur(userName);								
+			    }else if(saisie.equals(VueTexte.ADDIA) == true) {
+			    	jeu.addIA();
+			    }else if(saisie.equals(VueTexte.PLAY) == true) {
+			    	jeu.start();
+			    }else if(saisie.equals(VueTexte.PIOCHER) == true) {
+			    	jeu.getJoueurEnCours().piocher();
+			    }else if(saisie.equals(VueTexte.PLACER) == true) {
+			    	jeu.getJoueurEnCours().placer(jeu.getJoueurEnCours().getHand().getCard(0));
+			    }else if(saisie.equals(VueTexte.DEPLACER) == true) {
+			    	jeu.getJoueurEnCours().deplacer();
+			    }else if(saisie.equals(VueTexte.FINDETOUR) == true) {
+			    	jeu.unlockJoueur();
+			    }else if(saisie.equals(VueTexte.QUITTER) == true) {
 					quitter = true;
-				} else { output.println("Commande non reconnue ...");}
+				}else { output.println("Commande non reconnue ...");}
 			}
 			
 		} while (quitter == false);
@@ -88,6 +115,18 @@ public class VueTexte implements Observer, Runnable {
 	public void update(Observable o, Object arg) {
 		if((o instanceof ThreadMsgBox)&&(arg instanceof String)) {
 			afficher((String) arg);
+		}
+		if(o  instanceof Jeu) {
+			Jeu jeu = (Jeu) o;
+			output.println("Le joueur " +jeu.getJoueurEnCours()+ " a ");
+			if(jeu.getJoueurEnCours().aPioche()) {
+				output.println("pioché !");
+			} else if(jeu.getJoueurEnCours().aPlace()) {
+				output.println("placé !");
+		    } else if(jeu.getJoueurEnCours().aDeplace()) {
+		    	output.println("déplacé !");
+		    }
+			
 		}
 		
 	}
